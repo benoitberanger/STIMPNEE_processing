c17066b4b3ccc7b7940fc4505200eb6606b8947b
Benoît Béranger <benoit.beranger@icm-institute.org>
2016-12-12 15:21:21 +0100

    Regression : separation off Procedure 1 and Procedure 2

--- Regression_batch.m
+++ Regression_batch.m
@@ -8,7 +8,8 @@ clc
 
 maindir = '/media/benoit/DATADRIVE1/fMRI_data_benoit/STIMPNEE';
 
-designdir = get_subdir_regex(maindir,'Analyse_2ndlevel','Regression')
+designdir_P1 = get_subdir_regex(maindir,'Analyse_2ndlevel','Regression_P1')
+designdir_P2 = get_subdir_regex(maindir,'Analyse_2ndlevel','Regression_P2')
 
 imagepath = get_subdir_regex(maindir,'img')
 
@@ -31,42 +32,49 @@ myContrasts = {
 
 myScans = {
     
-% fmri exam       AMPL
+% fmri exam       AMPL   Procedure
 
-'Temoin02_V5_S1'  -45.5
-'Temoin02_V5_S2'  -38.2
-'Temoin03_V5_S1'  -54.2
-'Temoin03_V5_S2'  266.3
-'Temoin04_V5_S1'  147.7
-'Temoin04_V5_S2'  107.4
-'Temoin05_V5_S1'  -1.9
-'Temoin05_V5_S2'  -51.5
-'Temoin06_V5_S1'  -71.5
-'Temoin06_V5_S2'  22.4
-'Temoin07_V5_S1'  50.8
-'Temoin07_V5_S2'  34.2
+'Temoin02_V5_S1'  -45.5  1
+'Temoin02_V5_S2'  -38.2  2
+'Temoin03_V5_S1'  -54.2  2
+'Temoin03_V5_S2'  266.3  1
+'Temoin04_V5_S1'  147.7  2
+'Temoin04_V5_S2'  107.4  1
+'Temoin05_V5_S1'   -1.9  2
+'Temoin05_V5_S2'  -51.5  1
+'Temoin06_V5_S1'  -71.5  1
+'Temoin06_V5_S2'   22.4  2
+'Temoin07_V5_S1'   50.8  2
+'Temoin07_V5_S2'   34.2  1   
 % 'Temoin08_V5_S1'  x
 % 'Temoin08_V5_S2'  x
 
 };
 
-contrastpath = get_subdir_regex(imagepath,myScans(:,1),'stat','fMRI');
-char(contrastpath), size(contrastpath)
+% procedure = 1 => yellow, procedure = 2 => blue
 
-contrastfile = get_subdir_regex_files(contrastpath,'con_0009.nii');
-char(contrastfile), size(contrastfile)
+Procedure_1_idx = cell2mat(myScans(:,3)) == 1;
+Procedure_2_idx = cell2mat(myScans(:,3)) == 2;
+
+myScans_P1 = myScans(Procedure_1_idx,:);
+myScans_P2 = myScans(Procedure_2_idx,:);
+
+contrastpath_P1 = get_subdir_regex(imagepath,myScans_P1(:,1),'stat','fMRI');
+char(contrastpath_P1), size(contrastpath_P1)
+contrastpath_P2 = get_subdir_regex(imagepath,myScans_P2(:,1),'stat','fMRI');
+char(contrastpath_P2), size(contrastpath_P2)
+
+contrastfile_P1 = get_subdir_regex_files(contrastpath_P1,'con_0009.nii');
+char(contrastfile_P1), size(contrastfile_P1)
+contrastfile_P2 = get_subdir_regex_files(contrastpath_P2,'con_0009.nii');
+char(contrastfile_P2), size(contrastfile_P2)
 
 
 %% Fill the 2nd lvl design job
 
-%-----------------------------------------------------------------------
-% Job saved on 06-Dec-2016 11:29:14 by cfg_util (rev $Rev: 6460 $)
-% spm SPM - SPM12 (6685)
-% cfg_basicio BasicIO - Unknown
-%-----------------------------------------------------------------------
-job1{1}.spm.stats.factorial_design.dir = designdir;
-job1{1}.spm.stats.factorial_design.des.mreg.scans = contrastfile';
-job1{1}.spm.stats.factorial_design.des.mreg.mcov.c = cell2mat(myScans(:,2));
+job1{1}.spm.stats.factorial_design.dir = designdir_P1;
+job1{1}.spm.stats.factorial_design.des.mreg.scans = contrastfile_P1';
+job1{1}.spm.stats.factorial_design.des.mreg.mcov.c = cell2mat(myScans_P1(:,2));
 job1{1}.spm.stats.factorial_design.des.mreg.mcov.cname = 'AMPL';
 job1{1}.spm.stats.factorial_design.des.mreg.mcov.iCC = 1;
 job1{1}.spm.stats.factorial_design.des.mreg.incint = 1;
@@ -79,6 +87,20 @@ job1{1}.spm.stats.factorial_design.globalc.g_omit = 1;
 job1{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
 job1{1}.spm.stats.factorial_design.globalm.glonorm = 1;
 
+job1{2}.spm.stats.factorial_design.dir = designdir_P2;
+job1{2}.spm.stats.factorial_design.des.mreg.scans = contrastfile_P2';
+job1{2}.spm.stats.factorial_design.des.mreg.mcov.c = cell2mat(myScans_P2(:,2));
+job1{2}.spm.stats.factorial_design.des.mreg.mcov.cname = 'AMPL';
+job1{2}.spm.stats.factorial_design.des.mreg.mcov.iCC = 1;
+job1{2}.spm.stats.factorial_design.des.mreg.incint = 1;
+job1{2}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+job1{2}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+job1{2}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+job1{2}.spm.stats.factorial_design.masking.im = 1;
+job1{2}.spm.stats.factorial_design.masking.em = {''};
+job1{2}.spm.stats.factorial_design.globalc.g_omit = 1;
+job1{2}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+job1{2}.spm.stats.factorial_design.globalm.glonorm = 1;
 
 spm('defaults', 'FMRI');
 % spm_jobman('interactive', job1);
@@ -86,18 +108,25 @@ spm('defaults', 'FMRI');
 
 %% Prepare the job for estimation
 
-do_delete(designdir)
-mkdir(designdir{1})
+do_delete(designdir_P1)
+do_delete(designdir_P2)
+mkdir(designdir_P1{1})
+mkdir(designdir_P2{1})
 spm_jobman('run', job1);
 
 
 %% Estimate
 
-fspm = get_subdir_regex_files( designdir , 'SPM.mat' , 1 )
+fspm_P1 = get_subdir_regex_files( designdir_P1 , 'SPM.mat' , 1 )
+fspm_P2 = get_subdir_regex_files( designdir_P2 , 'SPM.mat' , 1 )
 
-job2{1}.spm.stats.fmri_est.spmmat = fspm ;
+job2{1}.spm.stats.fmri_est.spmmat = fspm_P1 ;
 job2{1}.spm.stats.fmri_est.write_residuals = 0;
 job2{1}.spm.stats.fmri_est.method.Classical = 1;
+
+job2{2}.spm.stats.fmri_est.spmmat = fspm_P2 ;
+job2{2}.spm.stats.fmri_est.write_residuals = 0;
+job2{2}.spm.stats.fmri_est.method.Classical = 1;
 
 
 %%
@@ -124,17 +153,18 @@ par.run=1;
 
 %%
 
-job_first_level12_contrast(fspm,contrast,par)
+job_first_level12_contrast(fspm_P1,contrast,par)
+job_first_level12_contrast(fspm_P2,contrast,par)
 
 
 %% Prepare show
 
-show{1}.spm.stats.results.spmmat = fspm;
+show{1}.spm.stats.results.spmmat = fspm_P1;
 show{1}.spm.stats.results.conspec.titlestr = '';
 show{1}.spm.stats.results.conspec.contrasts = 1;
 show{1}.spm.stats.results.conspec.threshdesc = 'none'; % 'none' 'FWE' 'FDR'
 show{1}.spm.stats.results.conspec.thresh = 0.05;
-show{1}.spm.stats.results.conspec.extent = 0;
+show{1}.spm.stats.results.conspec.extent = 10;
 show{1}.spm.stats.results.conspec.conjunction = 1;
 show{1}.spm.stats.results.conspec.mask.none = 1;
 show{1}.spm.stats.results.units = 1;
