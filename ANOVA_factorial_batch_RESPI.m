86e7f5fe306713e70f2e7e657c253a67c5d796db
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-03-08 13:46:10 +0100

    update of randomization of subjects

--- ANOVA_factorial_batch_RESPI.m
+++ ANOVA_factorial_batch_RESPI.m
@@ -29,72 +29,7 @@ myContrasts = {
     };
 
 
-myScans = {
-    
-% fmri exam       'procedure' 'time'
-
-% 'Temoin02_V1_S1'  1           1
-% 'Temoin02_V5_S1'  1           2
-% 'Temoin02_V1_S2'  2           1
-% 'Temoin02_V5_S2'  2           2
-% 'Temoin03_V1_S1'  2           1
-% 'Temoin03_V5_S1'  2           2
-% 'Temoin03_V1_S2'  1           1
-% 'Temoin03_V5_S2'  1           2
-'Temoin04_V1_S1'  2           1
-'Temoin04_V5_S1'  2           2
-'Temoin04_V1_S2'  1           1
-'Temoin04_V5_S2'  1           2
-% 'Temoin05_V1_S1'  2           1
-% 'Temoin05_V5_S1'  2           2
-% 'Temoin05_V1_S2'  1           1
-% 'Temoin05_V5_S2'  1           2
-'Temoin06_V1_S1'  1           1
-'Temoin06_V5_S1'  1           2
-'Temoin06_V1_S2'  2           1
-'Temoin06_V5_S2'  2           2
-% 'Temoin07_V1_S1'  2           1
-% 'Temoin07_V5_S1'  2           2
-% 'Temoin07_V1_S2'  1           1
-% 'Temoin07_V5_S2'  1           2
-'Temoin08_V1_S1'  1           1
-'Temoin08_V5_S1'  1           2
-'Temoin08_V1_S2'  2           1
-'Temoin08_V5_S2'  2           2
-'Temoin09_V1_S2'  2           1
-'Temoin09_V5_S2'  2           2
-'Temoin10_V1_S2'  1           1
-'Temoin10_V5_S2'  1           2
-'Temoin11_V1_S1'  2           1
-'Temoin11_V5_S1'  2           2
-'Temoin12_V1_S1'  1           1
-'Temoin12_V5_S1'  1           2
-'Temoin13_V1_S1'  2           1
-'Temoin13_V5_S1'  2           2
-'Temoin12_V1_S2'  2           1
-'Temoin12_V5_S2'  2           2
-'Temoin11_V1_S2'  1           1
-'Temoin11_V5_S2'  1           2
-'Temoin13_V1_S2'  1           1
-'Temoin13_V5_S2'  1           2
-'Temoin14_V1_S1'  2           1
-'Temoin14_V5_S1'  2           2
-'Temoin15_V1_S1'  1           1
-'Temoin15_V5_S1'  1           2
-'Temoin16_V1_S1'  1           1
-'Temoin16_V5_S1'  1           2
-'Temoin17_V1_S1'  2           1
-'Temoin17_V5_S1'  2           2
-'Temoin15_V1_S2'  2           1
-'Temoin15_V5_S2'  2           2
-'Temoin14_V1_S2'  1           1
-'Temoin14_V5_S2'  1           2
-'Temoin16_V1_S2'  2           1
-'Temoin16_V5_S2'  2           2
-'Temoin17_V1_S2'  1           1
-'Temoin17_V5_S2'  1           2
-
-};
+myScans = rando;
 
 % procedure = 1 => yellow, procedure = 2 => blue
 
@@ -196,8 +131,6 @@ job2{1}.spm.stats.fmri_est.method.Classical = 1;
 
 spm_jobman('run', job2);
 
-
-return
 
 %% Contraste
 
---
+++ rando.m
@@ -0,0 +1,111 @@
+function out = rando
+
+out = {
+    
+% fmri exam       'procedure' 'time'
+
+'Temoin01_V1_S1BIS' 1         1
+'Temoin01_V5_S1BIS' 1         2
+'Temoin01_V1_S2BIS' 2         1
+'Temoin01_V5_S2bis' 2         2
+
+% 'Temoin02_V1_S1'  1           1
+% 'Temoin02_V5_S1'  1           2
+% 'Temoin02_V1_S2'  2           1
+% 'Temoin02_V5_S2'  2           2
+
+% 'Temoin03_V1_S1'  2           1
+% 'Temoin03_V5_S1'  2           2
+% 'Temoin03_V1_S2'  1           1
+% 'Temoin03_V5_S2'  1           2
+
+'Temoin04_V1_S1'  2           1
+'Temoin04_V5_S1'  2           2
+'Temoin04_V1_S2'  1           1
+'Temoin04_V5_S2'  1           2
+
+% 'Temoin05_V1_S1'  2           1
+% 'Temoin05_V5_S1'  2           2
+% 'Temoin05_V1_S2'  1           1
+% 'Temoin05_V5_S2'  1           2
+
+'Temoin06_V1_S1'  1           1
+'Temoin06_V5_S1'  1           2
+'Temoin06_V1_S2'  2           1
+'Temoin06_V5_S2'  2           2
+
+'Temoin07_V1_S1BIS'  2           1
+'Temoin07_V5_S1BIS'  2           2
+'Temoin07_V1_S2'  1           1
+'Temoin07_V5_S2'  1           2
+
+'Temoin08_V1_S1'  1           1
+'Temoin08_V5_S1'  1           2
+'Temoin08_V1_S2'  2           1
+'Temoin08_V5_S2'  2           2
+
+'Temoin09_V1_S1'  1           1
+'Temoin09_V5_S1'  1           2
+'Temoin09_V1_S2'  2           1
+'Temoin09_V5_S2'  2           2
+
+'Temoin10_V1_S1'  2           1
+'Temoin10_V5_S1'  2           2
+'Temoin10_V1_S2'  1           1
+'Temoin10_V5_S2'  1           2
+
+'Temoin11_V1_S1'  2           1
+'Temoin11_V5_S1'  2           2
+'Temoin11_V1_S2'  1           1
+'Temoin11_V5_S2'  1           2
+
+'Temoin12_V1_S1'  1           1
+'Temoin12_V5_S1'  1           2
+'Temoin12_V1_S2'  2           1
+'Temoin12_V5_S2'  2           2
+
+'Temoin13_V1_S1'  2           1
+'Temoin13_V5_S1'  2           2
+'Temoin13_V1_S2'  1           1
+'Temoin13_V5_S2'  1           2
+
+'Temoin14_V1_S1'  2           1
+'Temoin14_V5_S1'  2           2
+'Temoin14_V1_S2'  1           1
+'Temoin14_V5_S2'  1           2
+
+'Temoin15_V1_S1'  1           1
+'Temoin15_V5_S1'  1           2
+'Temoin15_V1_S2'  2           1
+'Temoin15_V5_S2'  2           2
+
+'Temoin16_V1_S1'  1           1
+'Temoin16_V5_S1'  1           2
+'Temoin16_V1_S2'  2           1
+'Temoin16_V5_S2'  2           2
+
+'Temoin17_V1_S1'  2           1
+'Temoin17_V5_S1'  2           2
+'Temoin17_V1_S2'  1           1
+'Temoin17_V5_S2'  1           2
+
+'Temoin18_V1_S1'  1           1
+'Temoin18_V5_S1'  1           2
+'Temoin18_V1_S2'  2           1
+'Temoin18_V5_S2'  2           2
+
+'Temoin19_V1_S1'  1           1
+'Temoin19_V5_S1'  1           2
+'Temoin19_V1_S2'  2           1
+'Temoin19_V5_S2'  2           2
+
+'Temoin20_V1_S1'  2           1
+'Temoin20_V5_S1'  2           2
+'Temoin20_V1_S2'  1           1
+'Temoin20_V5_S2'  1           2
+
+};
+
+% procedure = 1 => yellow, procedure = 2 => blue
+
+end % function
